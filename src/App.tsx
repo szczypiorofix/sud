a821f4c9379494537d27cae32f1ee2a4cda3d9a7
szczypiorofix <szczypiorofix@o2.pl>
2022-12-16 08:51:34 +0100

    Preparing for useReducer hook.

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,63 @@
-import React, {useState} from 'react';
-import {IAppState, STAGE} from "./App.model";
-import {SplashScreen} from "./stages/splashscreen/SplashScreen";
-import {MainMenu} from "./stages/mainmenu/MainMenu";
+import React, {useReducer} from 'react';
+import {STAGE} from "./App.model";
 
 import './App.scss';
 import MainAppContext, {useGlobalContext} from "./core/MainAppContext";
-import {NewGame} from "./stages/newgame/NewGame";
+import {SplashScreen} from "./stages/splashscreen/SplashScreen";
+
+
+export interface IReducerState {
+    stage: STAGE;
+}
+
+
+export enum REDUCER_ACTION_TYPE {
+    MOVE_TO_MAIN_MENU,
+    MOVE_TO_NEW_GAME
+}
+
+
+export interface IReducerAction {
+    type: REDUCER_ACTION_TYPE;
+    payload: STAGE;
+}
+
+
+const reducer = (state: IReducerState , action: IReducerAction) => {
+    const { type, payload } = action;
+    switch( type ) {
+        case REDUCER_ACTION_TYPE.MOVE_TO_MAIN_MENU:
+            return { ...state, value: state.stage = payload };
+        default:
+            return state;
+    }
+}
 
 
 export const App: React.FC = () => {
 
     const { stage, setStage } = useGlobalContext();
+    const [state, dispatch] = useReducer( reducer, { stage: STAGE.SPLASH } );
 
-    const [ state, setState ] = useState<IAppState>({
-        stage: STAGE.SPLASH,
-        name: "splash"
-    });
+
+    // const [ state, setState ] = useState<IAppState>({
+    //     stage: STAGE.SPLASH,
+    //     name: "splash"
+    // });
 
 
     const stageResolver = () => {
-        switch( state.stage ) {
-            case STAGE.MAIN_MENU:
-                return <MainMenu />
-            case STAGE.NEW_GAME:
-                return <NewGame />
-            default:
-                return <SplashScreen />
-        }
+        // switch( state.stage ) {
+        //     case STAGE.MAIN_MENU:
+        //         return <MainMenu />
+        //     case STAGE.NEW_GAME:
+        //         return <NewGame />
+        //     default:
+        //         return <SplashScreen />
+        // }
+
+        return <SplashScreen />
+
     }
 
 
@@ -36,13 +67,15 @@ export const App: React.FC = () => {
     return <div className='background-container'>
         <MainAppContext.Provider
             value={ {
-                stage: state.stage,
-                setStage: (stage: STAGE) => {
-                    setState({
-                        ...state,
-                        stage: stage
-                    });
-                }
+                // stage: state.stage,
+                // setStage: (stage: STAGE) => {
+                //     setState({
+                //         ...state,
+                //         stage: stage
+                //     });
+                // }
+                stage:STAGE.SPLASH,
+                setStage: () => {}
             } }
         >
             { stageResolver() }
